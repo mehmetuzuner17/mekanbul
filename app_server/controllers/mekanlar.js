a1f1bdf3b7cba48f58592df3c432438384b4de41
erenshen <erensh4n@gmail.com>
2022-10-20 15:59:07 +0300

    20.10.2022

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,12 +1,66 @@
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
-const anaSayfa = function (req, res, next) {
-  res.render('anasayfa', { 'title': 'Ana Sayfa' });
+const anaSayfa = function (req, res) {
+  res.render('anasayfa', { "baslik": "Ana Sayfa","sayfaBaslik": {
+    "siteAd":"MekanBul",
+    "slogan":"Civardaki Mekanları Keşfet!"
+  },
+  "mekanlar": [
+    {
+      "ad":"Starbucks",
+      "adres":"Centrum Garden AVM",
+      "puan":"4",
+      "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
+      "mesafe":"10km"
+    }
+    ,
+    {
+      "ad":"Gloria Jeans",
+      "adres":"SDÜ Doğu Kampüsü",
+      "puan":"3",
+      "imkanlar": ["Kahve","Çay","Pasta"],
+      "mesafe":"5km"
+    }
+  ]
+ });
 }
 
-const mekanBilgisi = function (req, res, next) {
-  res.render('mekanbilgisi', { 'title': 'Mekan bilgisi' });
+const mekanBilgisi = function (req, res) {
+  res.render('mekanbilgisi', {
+     "baslik": "Mekan bilgisi",
+      "mekanBaslik": "Starbucks",
+      "mekanDetay": {
+        "ad":"Starbucks",
+        "adres":"Centrum Garden AVM",
+        "puan":"4",
+        "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
+        "koordinatlar": {
+           "enlem":"37.7",
+           "boylam":"38.5"
+        },
+        "saatler": [
+          {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"09.00",
+            "kapanis":"23.00",
+            "kapali": false
+          },
+          {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"10.00",
+            "kapanis":"22.00",
+            "kapali": false
+          }
+        ],
+        "yorumlar": [
+          {
+            "yorumYapan":"Eren Şen",
+            "puan":"4",
+            "tarih":"20 Ekim 2022",
+            "yorumMetni":"Kahveler enfes!"
+
+          }
+        ]
+      }
+    });
 }
 
 const yorumEkle = function (req, res, next) {
