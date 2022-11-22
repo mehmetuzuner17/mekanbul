35a7c9c4d9b313cfabe331ce818586367878ec02
erenshen <erensh4n@gmail.com>
2022-11-22 16:23:20 +0300

    odev5 eklendiii

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("./mekansema");
-//var dbURI ="mongodb+srv://mekan32:mekan32@mekanbul.i7z3b1e.mongodb.net/?retryWrites=true&w=majority"
-var dbURI = 'mongodb://127.0.0.1/mekanbul';
+var dbURI ="mongodb+srv://mekan32:mekan32@mekanbul.i7z3b1e.mongodb.net/?retryWrites=true&w=majority"
+//var dbURI = 'mongodb://127.0.0.1/mekanbul';
 
 mongoose.connect(dbURI);
 function kapat(msg,callback){
