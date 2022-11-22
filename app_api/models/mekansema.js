c963efd70851817b631057a0cf49c7cfbf48f066
erenshen <erensh4n@gmail.com>
2022-11-22 15:20:56 +0300

    odev5 eklendiii

--- app_api/models/mekansema.js
+++ app_api/models/mekansema.js
@@ -1,24 +1,23 @@
-var mongoose=require("mongoose");
-
+var mongoose = require("mongoose");
 var saatSema = new mongoose.Schema({
-    gunler:{type:String, required:true},
-    acilis:String,
-    kapanis:String, 
-    kapali:{type:Boolean},
+    gunler: { type: String, required: true },
+    acilis: String,
+    kapanis: String,
+    kapali: { type: Boolean }
 });
 var yorumSema = new mongoose.Schema({
-    yorumYapan:{type:String,required:true},
-    puan:{type:Number, default:0,min:0,max:5},
-    yorumMetni:{type:String,required:true},
-    tarih:{type:Date, default:Date.now},
+    yorumYapan: { type: String, required: true },
+    puan: { type: Number, default: 0, min: 0, max: 5 },
+    yorumMetni: { type: String, required: true },
+    tarih: { type: Date, default: Date.now }
 });
 var mekanSema = new mongoose.Schema({
-    ad:{type:String, required:true},
-    adres:String,
-    puan:{type:Number, default:0, min:0, max:5},
-    imkanlar:[String],
-    koordinat:{type:[Number], index:'2dsphere'},
+    ad: { type: String, required: true },
+    adres: String,
+    puan: { type: Number, default: 0, min: 0, max: 5 },
+    imkanlar: [String],
+    koordinat: { type: [Number], index: '2dsphere' },
     saatler:[saatSema],
-    yorumlar:[yorumSema],
+    yorumlar:[yorumSema]
 });
-mongoose.model("mekan",mekanSema,"mekanlar");
+mongoose.model("Mekan",mekanSema,"mekanlar");
